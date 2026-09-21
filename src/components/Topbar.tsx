425030252b507bc3262a46be1f03ed655086ec23
Amanda Vieira <amandavp23@gmail.com>
2026-09-21 20:53:13 +0100

    Logo in topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,6 +1,9 @@
+import FullLogo from '../assets/icons/logos/logo-full-color.svg';
+
 function Topbar() {
     return (
         <div className="topbar">
+            <FullLogo />
             <h1>Hello!</h1>
         </div>
     );
